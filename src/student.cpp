c4dee0b016efe4fce3aa1d1702de5761284ba598
yuyansong <1259581033@qq.com>
2025-04-25 21:47:09 +0800

    first commit

---
+++ src/student.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+#include<ctime>
+using namespace std;
+struct Hero
+{
+	string name;
+	int age;
+	string categories;
+};
+int main(void)
+{	
+	struct Hero heroArry[5] =
+	{
+		{"欣南",20,"火"},
+		{"东杉",24,"木"},
+		{"北淼",23,"水"},
+		{"坤中",18,"土"},
+		{"西昭",22,"金"},
+	};
+	int len = sizeof(heroArry) / sizeof(heroArry[0]);
+	for (int i = 0; i < len - 1; i++)
+	{
+		for (int j = 0; j < len - 1 - i; j++)
+		{
+			Hero temp;
+			if (heroArry[j].age > heroArry[j + 1].age)
+			{
+				temp = heroArry[j];
+				heroArry[j] = heroArry[j + 1];
+				heroArry[j + 1] = temp;
+			}
+		}
+	}
+	for (int i = 0; i <len; i++)
+	{
+		cout << heroArry[i].name << heroArry[i].age << heroArry[i].categories << endl;
+	}
+	system("pause");
+	return 0;
+}
---
+++ src/teacher.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<string>
+#include<ctime>
+using namespace std;
+struct Student
+{
+	string name;
+	int age;
+	int score;
+};
+struct Teacher
+{
+	string name;
+	struct Student sArry[5];
+};
+void inPutInformation(struct Teacher tArry[], int len)
+{
+	string Name = "ABCDE";
+	for (int i = 0; i < len; i++)
+	{
+		tArry[i].name = "Teacher_";
+		tArry[i].name += Name[i];
+		for (int j = 0; j < 5; j++)
+		{
+			tArry[i].sArry[j].name = "Student_";
+			tArry[i].sArry[j].name += Name[j];
+			int random = rand()% 60 +40;
+			tArry[i].sArry[j].score = random;
+		}
+	}
+}
+void printInformation(struct Teacher tArry[],int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		cout << "老师的姓名：" << tArry[i].name << endl;
+		for (int j = 0; j < 5; j++)
+		{
+			cout << "\t学生的姓名：" << tArry[i].sArry[j].name << "考试分数：" << tArry[i].sArry[j].score << endl;
+		}
+	}
+}
+int main(void)
+{	
+	srand((unsigned int)time(NULL));
+	struct Teacher tArry[3];
+	int len = sizeof(tArry) / sizeof(tArry[0]);
+	inPutInformation(tArry,len);
+	printInformation(tArry,len);
+	system("pause");
+	return 0;
+}
