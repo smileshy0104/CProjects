ab64240fbdc3fb40557c3c519e5fd3a3009d32e1
yuyansong <1259581033@qq.com>
2025-04-29 08:01:57 +0800

    使用 ios::out 表示以写模式打开文件

--- src/file_demo/file_write.cpp
+++ src/file_demo/file_write.cpp
@@ -1,23 +1,42 @@
-#include <fstream>
-#include <iostream>
+#include <fstream>   // 包含文件流库，用于文件操作
+#include <iostream>  // 包含输入输出流库，用于控制台输入输出
 using namespace std;
-// 文本文件写文件
+
+//* 文本文件写文件
+/*
+- 文件操作必须包含头文件 fstream
+- 写文件可以利用 ofstream 类
+- 打开文件时需要指定操作文件的路径，以及打开方式
+- 利用 << 可以向文件中写数据
+- 操作完毕后，要关闭文件
+*/
+
 void test01() {
-  // 1.包含头文件
-  // 2.创建流对象
+  // 1. 创建 ofstream 对象，用于文件写操作
   ofstream ofs;
-  // 3.指定打开方式
-  ofs.open("test.txt",
-           ios::out);  // 如果不指定文件路径，默认和你项目的文件路径一样
-  // 4.写内容
-  ofs << "姓名:张三" << endl;
-  ofs << "性别:男" << endl;
-  ofs << "年龄:18" << endl;
-  // 5.关闭文件
+
+  // 2. 打开文件，指定文件路径和打开方式
+  //    这里使用 ios::out 表示以写模式打开文件
+  //    如果文件不存在，会创建新文件；如果文件存在，会覆盖原有内容
+  ofs.open("test.txt", ios::out);
+
+  // 3. 检查文件是否成功打开
+  if (!ofs.is_open()) {
+    cerr << "无法打开文件 test.txt" << endl;
+    return;
+  }
+
+  // 4. 向文件中写入内容
+  ofs << "姓名:张三2" << endl;  // 写入姓名
+  ofs << "性别:男" << endl;     // 写入性别
+  ofs << "年龄:18" << endl;     // 写入年龄
+
+  // 5. 关闭文件，释放资源
   ofs.close();
 }
+
 int main(void) {
-  test01();
-  system("pause");
-  return 0;
+  test01();         // 调用 test01 函数进行文件写操作
+  system("pause");  // 暂停程序，等待用户按键（便于观察输出）
+  return 0;         // 返回 0 表示程序正常结束
 }
